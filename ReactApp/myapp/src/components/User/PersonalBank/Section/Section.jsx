e07baad6d360767d9d4bfbc27dd4e356f34847d1
CapronLee <kasamata2.2003@gmail.com>
2024-02-19 16:05:49 +0700

    khongcogi

--- ReactApp/myapp/src/components/User/PersonalBank/Section/Section.jsx
+++ ReactApp/myapp/src/components/User/PersonalBank/Section/Section.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useParams, NavLink } from 'react-router-dom';
-import axios, { all } from 'axios'
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import { NavLink, useParams } from 'react-router-dom';
+import MyEditor from '../../../MyEditor';
 import '../Section/Section.css';
 
-import Question from '../Question/Question';
 
-const Section = () => {
+const Section = (props) => {
   // Sử dụng useParams để trích xuất tham số bid từ URL
   const { bid } = useParams();
   const [data, setData] = useState([]);
@@ -14,7 +15,87 @@ const Section = () => {
   const [selectedQuestions, setSelectedQuestions] = useState([]);
   const [questype, setQuestype] = useState('3');
   const [selectedSection, setSelectedSection] = useState();
+  const [showModal1, setShowModal1] = useState(false);
+  const [showModal2, setShowModal2] = useState(false);
+  const [questions, setQuestions] = useState([
+    {
+      id: '1',
+      title: 'Đề ở đây',
+      answer1: 'Đáp án ở đây',
+      answer2: 'Đáp án ở đây',
+      answer3: 'đáp án ở đây',
+      answer4: 'đáp án ở đây',
+      checkbox: ''
+    }
+  ]);
+  const [editingQuestionId, setEditingQuestionId] = useState(null);
 
+  const handleAddQuestion = () => {
+    const newId = (questions.length + 1).toString();
+    setQuestions([
+      ...questions,
+      {
+        id: newId,
+        title: 'Đề ở đây',
+        answer1: 'Đáp án ở đây',
+        answer2: 'Đáp án ở đây',
+        answer3: 'đáp án ở đây',
+        answer4: 'đáp án ở đây',
+        checkbox: ''
+      },
+    ]);
+  };
+
+  const handleEditQuestion = (questionId) => {
+    setEditingQuestionId(questionId);
+    // Thực hiện các xử lý khác nếu cần
+  };
+
+  const handleEditAnswer = (questionId, answerIndex, newAnswer) => {
+    setQuestions((prevQuestions) =>
+      prevQuestions.map((question) =>
+        question.id === questionId ? { ...question, [`answer${answerIndex + 1}`]: newAnswer } : question
+      )
+    );
+  };
+
+  const handleSaveEdit = (questionId) => {
+    // Thực hiện lưu chỉnh sửa cho câu hỏi
+    console.log('Lưu chỉnh sửa cho câu hỏi có ID:', questionId);
+
+    // Đặt trạng thái chỉnh sửa về mặc định
+    setEditingQuestionId(null);
+  };
+
+  const handleEditTitle = (questionId, newTitle) => {
+    setQuestions((prevQuestions) =>
+      prevQuestions.map((question) =>
+        question.id === questionId ? { ...question, title: newTitle } : question
+      )
+    );
+  };
+
+  const handleDeleteQuestion = (questionId) => {
+    const updatedQuestions = questions.filter((question) => question.id !== questionId);
+    setQuestions(updatedQuestions);
+    setEditingQuestionId(null); // Đặt editingQuestionId về null nếu câu hỏi đang được chỉnh sửa bị xóa
+  };
+
+  const close1 = () => {
+    setShowModal1(false);
+  };
+
+  const open1 = () => {
+    setShowModal1(true);
+  };
+
+  const close2 = () => {
+    setShowModal2(false);
+  };
+
+  const open2 = () => {
+    setShowModal2(true);
+  };
   const fetchDataFromApi = async () => {
     try {
       const resq = await axios.post('https://localhost:7064/api/Bank/vquestions', {
@@ -160,17 +241,144 @@ const Section = () => {
               <span className='selectedq'>{selectedQuestions.length} được chọn</span>
               <span className='tool-item'><i className="fa-solid fa-pen-to-square"></i></span>
               <span className='tool-item' onClick={deleteQuestions}><i className="fa-solid fa-trash-can"></i></span>
-              <span className='tool-item'><i class="fa-solid fa-plus"></i></span>
-              <span className='tool-item'><i class="fa-regular fa-file-word"></i></span>
-              <span className='tool-item'><i class="fa-regular fa-file-excel"></i></span>
+              <span className='tool-item' onClick={handleAddQuestion}><i class="fa-solid fa-plus"></i></span>
+              <span className='tool-item' onClick={open2}><i class="fa-regular fa-file-word"></i></span>
+              <span className='tool-item' onClick={open1}><i class="fa-regular fa-file-excel"></i></span>
             </div>
 
             <div className='qlistitem'>
-              {data.map((question) => (
-                <div className={`qcontainer ${selectedQuestions.includes(question.questionId) ? 'selected' : ''}`} key={question.questionId} onClick={() => selectQuestionsHandle(question.questionId)}><Question props={question}></Question></div>
+              {questions.map((question, index) => (
+                // <div className={`qcontainer ${selectedQuestions.includes(question.questionId) ? 'selected' : ''}`} key={question.questionId} onClick={() => selectQuestionsHandle(question.questionId)}><Question props={question}></Question></div>
+                <React.Fragment key={index}>
+                  {editingQuestionId === question.id ? (
+                    /* Hiển thị nội dung chỉnh sửa */
+                    <div className="edit-question" key={index}>
+                      <label htmlFor={`editTitle_${question.id}`}>Đề:</label>
+                      <input
+                        type="text"
+                        id={`editTitle_${question.id}`}
+                        value={question.title}
+                        onChange={(e) => handleEditTitle(question.id, e.target.value)}
+                      />
 
+                      <table className="edit-answers">
+                        <thead>
+                          <tr>
+                            <th>Đáp án</th>
+                            <th>Nội dung đáp án</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {['answer1', 'answer2', 'answer3', 'answer4'].map((answerKey, answerIndex) => (
+                            <tr key={answerIndex}>
+                              <td>Đáp án {answerIndex + 1}:</td>
+                              <td>
+                                <input
+                                  type="text"
+                                  id={`editAnswer_${question.id}_${answerIndex + 1}`}
+                                  value={question[answerKey]}
+                                  onChange={(e) => handleEditAnswer(question.id, answerIndex, e.target.value)}
+                                />
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+
+                      {/* Thêm các trường chỉnh sửa khác cho câu hỏi ở đây */}
+
+                      <button onClick={() => handleSaveEdit(question.id)}>
+                        Lưu chỉnh sửa
+                      </button>
+                    </div>
+                  ) : (
+
+                    <table className="addquestion-table">
+                      <thead>
+                        <td className="row-head">
+                          <span className="left-row"> <i class="fa-solid fa-circle-question"></i> Câu hỏi {index + 1}</span>
+                          <span className="right-row">
+                            <button title="Chỉnh sửa" onClick={() => handleEditQuestion(question.id)}><i class="fa-solid fa-pen-to-square"></i>Chỉnh sửa</button>
+                            <button title="Nhân đôi câu hỏi này"><i class="fa-solid fa-copy"></i></button>
+                            <button title="Xóa câu hỏi này" onClick={() => handleDeleteQuestion(question.id)}><i class="fa-solid fa-trash-can"></i></button>
+                          </span>
+                        </td>
+                      </thead>
+                      <tr>
+                        <td>
+                          Đề: {questions[index].title}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          {/* <span className="text-over-border">lựa chọn trả lời</span> */}
+                          <div>
+                            <input type="checkbox" id="answer1" title="đánh dấu đáp án đúng" />
+                            <label htmlFor="answer1">Đáp án 1: </label>
+                            <label htmlFor="answer1">{questions[index].answer1}</label>
+                          </div>
+                          <div>
+                            <input type="checkbox" id="answer2" title="đánh dấu đáp án đúng" />
+                            <label htmlFor="answer2">Đáp án 2: </label>
+                            <label htmlFor="answer2">{questions[index].answer2}</label>
+                          </div>
+                          <div>
+                            <input type="checkbox" id="answer3" title="đánh dấu đáp án đúng" />
+                            <label htmlFor="answer3">Đáp án 3: </label>
+                            <label htmlFor="answer3">{questions[index].answer3}</label>
+                          </div>
+                          <div>
+                            <input type="checkbox" id="answer4" title="đánh dấu đáp án đúng" />
+                            <label htmlFor="answer4">Đáp án 4: </label>
+                            <label htmlFor="answer4">{questions[index].answer4}</label>
+                          </div>
+                        </td>
+                      </tr>
+                    </table>
+                  )}
+                  <br /><br />
+                </React.Fragment>
               ))}
+              <Modal show={showModal1} onHide={close1} className="excel-modal">
+                <Modal.Header closeButton className="modal-header">
+                  <div className="header-icon">
+                    <i class="fa-solid fa-table fa-2xl"></i>
+                  </div>
+                  <div>
+                    <h5>Nhập câu hỏi từ excel</h5>
+                    <div className="modal-subtitle">Vui lòng tải lên bảng tính excel theo mẫu</div>
+                  </div>
+                </Modal.Header>
+                <Modal.Body>
 
+                  <MyEditor />
+
+                </Modal.Body>
+                <Modal.Footer>
+                  <button>Tải về mẫu</button>
+                  <button onClick={close1}>Close</button>
+                </Modal.Footer>
+              </Modal>
+              <Modal show={showModal2} onHide={close2}>
+                <Modal.Header closeButton>
+                  <div className="header-icon">
+                    <i class="fa-regular fa-file-word fa-2xl"></i>
+                  </div>
+                  <div>
+                    <h5>Nhập câu hỏi từ Word</h5>
+                    <div className="modal-subtitle">Vui lòng tải lên Word theo định dạng mẫu</div>
+                  </div>
+                </Modal.Header>
+                <Modal.Body>
+
+                  <MyEditor />
+
+                </Modal.Body>
+                <Modal.Footer>
+                  <button>Tải về mẫu</button>
+                  <button onClick={close2}>Close</button>
+                </Modal.Footer>
+              </Modal>
             </div>
 
           </div>
@@ -184,10 +392,10 @@ const Section = () => {
 
             <div className="section-list">
               {section.map((sec) => (
-                <div key={sec.sectionId} className={`sectionitem ${selectedSection === sec.sectionId ? 'selected' : ''}`} onClick={()=>handleSelectSection(sec.sectionId)}>
-                {sec.secTitle}
-              </div>
-              ))}             
+                <div key={sec.sectionId} className={`sectionitem ${selectedSection === sec.sectionId ? 'selected' : ''}`} onClick={() => handleSelectSection(sec.sectionId)}>
+                  {sec.secTitle}
+                </div>
+              ))}
             </div>
 
           </div>
--- ReactApp/myapp/src/pages/Root.jsx
+++ ReactApp/myapp/src/pages/Root.jsx
@@ -1,14 +1,14 @@
 // src/screens/Root.jsx
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Thay thế 'Switch' bằng 'Routes'
+import Footer from '../components/UI/Footer';
+import Header from '../components/UI/Header';
 import HomeScreen from './User/Home';
 import PBanks from './User/PBank';
 import Profile from './User/Profile';
-import Sections from './User/Sections';
-import Qbanks from './User/Qbanks'
-import Header from '../components/UI/Header';
-import Footer from '../components/UI/Footer';
+import Qbanks from './User/Qbanks';
 import Repository from './User/Repository';
+import Sections from './User/Sections';
 
 const Root = () => (
   <BrowserRouter>
@@ -17,14 +17,14 @@ const Root = () => (
 
       {/* User Routes */}
       <Route path="/" element={<HomeScreen />} />
-      <Route path="/qbank" element={< Qbanks/>} />
+      <Route path="/qbank" element={< Qbanks />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/pbank" element={<PBanks />}/>
-      <Route path="/repo" element={<Repository />}/>
-      <Route path="/sec/:bid" element={<Sections />}/>
+      <Route path="/pbank" element={<PBanks />} />
+      <Route path="/repo" element={<Repository />} />
+      <Route path="/sec/:bid" element={<Sections />} />
       {/* <Route path="/qbank/pbank/:bid" element={<YourNestedComponent/>}/> */}
-        
-      
+
+
       {/* <Route path="/user/create" element={<UserFormScreen />} />
       <Route path="/user/:id" element={<UserFormScreen />} /> */}
 
