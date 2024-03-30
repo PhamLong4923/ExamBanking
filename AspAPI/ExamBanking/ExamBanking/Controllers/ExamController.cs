32ceea3a2c36fbcc6d4c2fb3a465a04cd870951a
unclvev <vietnhhe172276@fpt.edu.vn>
2024-03-30 23:07:31 +0700

    add feature random exam

---
+++ AspAPI/ExamBanking/ExamBanking/Controllers/ExamController.cs
@@ -0,0 +1,52 @@
+﻿using ExamBanking.Models;
+using ExamBanking.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamBanking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamController : ControllerBase
+    {
+        private readonly ExamBankingContext _context;
+
+
+        public ExamController(ExamBankingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("CreateExam")]
+        public IActionResult CreateExam(int section, int recognizeCount, int understandCount, int applyCount)
+        {
+            Random rnd = new Random();
+
+            //cảm ơn chatgpt
+            var selectedQuestions = new List<List<Question>>
+            {
+                GetRandomQuestions(section, 1, recognizeCount, rnd), 
+                GetRandomQuestions(section, 2, understandCount, rnd), 
+                GetRandomQuestions(section, 3, applyCount, rnd) 
+            };
+
+           
+            return Ok(selectedQuestions);
+        }
+
+        
+        private List<Question> GetRandomQuestions(int section, int modeId, int count, Random rnd)
+        {
+           
+            var questions = _context.Questions
+                .Where(x => x.Secid == section && x.Modeid == modeId)
+                .ToList();
+
+            
+            var randomizedQuestions = questions.OrderBy(x => rnd.Next()).Take(count).ToList();
+
+            return randomizedQuestions;
+        }
+
+    }
+}
