4046661c1b4d9fe7f6928929fcc4762868f98ca1
unclvev <vietnhhe172276@fpt.edu.vn>
2024-04-08 23:38:15 +0700

    show total level by section

--- AspAPI/ExamBanking/ExamBanking/Controllers/ExamController.cs
+++ AspAPI/ExamBanking/ExamBanking/Controllers/ExamController.cs
@@ -17,6 +17,64 @@ namespace ExamBanking.Controllers
             _context = context;
         }
 
+
+        [HttpGet("ShowExam")]
+        public IActionResult CreateExam(int repoId)
+        {
+            var selectedQuestions = new List<object>();
+
+            // Tìm repo từ repoid trong cơ sở dữ liệu
+            var repo = _context.Repos.FirstOrDefault(r => r.Repoid == repoId);
+
+            if (repo == null)
+            {
+                // Trả về NotFound nếu không tìm thấy repo
+                return NotFound($"Repo with ID '{repoId}' not found.");
+            }
+
+            var sections = _context.Sections.ToList(); // Lấy danh sách các section từ database
+
+            foreach (var sec in sections)
+            {
+                var sectionObject = new
+                {
+                    secid = sec.Secid,
+                    secname = sec.Secname,
+                    count = new List<object>()
+                };
+
+                var modeCounts = new List<object>();
+
+                // Lấy danh sách các modeid và tên mode tương ứng trong cơ sở dữ liệu
+                var modes = _context.Questions
+                                   .Where(q => q.Secid == sec.Secid)
+                                   .Select(q => new { q.Modeid, q.Mode.Qmode })
+                                   .Distinct()
+                                   .ToList();
+
+                foreach (var mode in modes)
+                {
+                    // Đếm số lượng câu hỏi cho từng modeid trong mỗi section
+                    var questionCount = _context.Questions.Count(q => q.Secid == sec.Secid && q.Modeid == mode.Modeid);
+
+                    modeCounts.Add(new { modename = mode.Qmode, count = questionCount });
+                }
+
+                sectionObject.count.Add(new { mcq = modeCounts });
+
+                selectedQuestions.Add(sectionObject);
+            }
+
+            return Ok(new
+            {
+                repoid = repoId, // Sử dụng repoid từ yêu cầu HTTP
+                reponame = repo.Reponame, // Sử dụng reponame từ repo được tìm thấy
+                secs = selectedQuestions
+            });
+        }
+
+
+
         [HttpPost("CreateExam")]
         public IActionResult CreateExam(int section, int recognizeCount, int understandCount, int applyCount)
         {
