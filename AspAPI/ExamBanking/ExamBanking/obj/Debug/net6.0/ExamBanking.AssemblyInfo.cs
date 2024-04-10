ec90ed724edc6da3595d884ff199460a4515e929
unclvev <vietnhhe172276@fpt.edu.vn>
2024-04-11 04:12:07 +0700

    funtion gg

--- AspAPI/ExamBanking/ExamBanking/obj/Debug/net6.0/ExamBanking.AssemblyInfo.cs
+++ AspAPI/ExamBanking/ExamBanking/obj/Debug/net6.0/ExamBanking.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamBanking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace6cc505f26e6c1a630eb0dca9cde73bbcd98e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3ef830203065cf82f18fad295c44a0a5e7ee5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamBanking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamBanking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
