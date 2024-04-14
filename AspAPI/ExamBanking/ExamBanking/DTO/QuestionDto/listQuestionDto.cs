e757fda6531eebdabc0670ad11bb707f82179d9c
unclvev <vietnhhe172276@fpt.edu.vn>
2024-04-15 02:45:38 +0700

    done task fixing list question and edit

---
+++ AspAPI/ExamBanking/ExamBanking/DTO/QuestionDto/listQuestionDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamBanking.DTO.QuestionDto
+{
+    public class listQuestionDto
+    {
+        public int quesid { get; set; }
+        public string? quescontent { get; set; }
+        public List<AnswerOutput>? answers { get; set; }
+        public string? solution { get; set; }
+        public int? type { get; set; }
+        public int? modeid { get; set; }
+    }
+}
