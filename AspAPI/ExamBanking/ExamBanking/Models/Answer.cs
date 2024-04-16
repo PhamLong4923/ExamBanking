0d30af2f9f66b40dc2f5e3ed1fc14da49a4b5ad1
unclvev <vietnhhe172276@fpt.edu.vn>
2024-04-17 01:45:18 +0700

    test

--- AspAPI/ExamBanking/ExamBanking/Models/Answer.cs
+++ AspAPI/ExamBanking/ExamBanking/Models/Answer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace ExamBanking.Models
 {
@@ -9,7 +10,7 @@ namespace ExamBanking.Models
         public string? Anscontent { get; set; }
         public byte? Ansstatus { get; set; }
         public int? Quesid { get; set; }
-
+        [JsonIgnore]
         public virtual Question? Ques { get; set; }
     }
 }
