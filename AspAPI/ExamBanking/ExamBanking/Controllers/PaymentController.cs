c28bf69b12cdc209bc4b833ac0e1d8ea68e9372b
unclvev <vietnhhe172276@fpt.edu.vn>
2024-04-22 00:46:36 +0700

    create payment

---
+++ AspAPI/ExamBanking/ExamBanking/Controllers/PaymentController.cs
@@ -0,0 +1,54 @@
+﻿using ExamBanking.DTO.PaymentDto;
+using ExamBanking.Models;
+using ExamBanking.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ExamBanking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "User")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly ExamBankingContext _context;
+        private readonly TicketController _ticket;
+        public PaymentController(ExamBankingContext context,TicketController ticket)
+        {
+            _context = context;
+            _ticket = ticket;
+        }
+        [HttpPost("createPayment")]
+        public async Task<IActionResult> CreatePayment(CreatePaymentRequest request)
+        {
+            var userId = Jwt.GetUserIdFromToken(Request.Headers["Authorization"]);
+            var user = _context.Accounts.SingleOrDefault(u => u.Email == userId);
+            var emailClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
+            if (emailClaim == null)
+            {
+                return BadRequest("Email claim not found in the token.");
+            }
+            if (user == null)
+            {
+                return Ok("User not found or token is invalid.");
+            }
+
+            var payment = new Payment
+            {
+                Money = request.Money,
+                Accid = user.Accid,
+                Paydate = DateTime.Now,
+                Status = 0,
+                Paycontent = request.Paycontent +"_"+ emailClaim.Value, 
+            };
+            
+            _context.Payments.Add(payment);
+            _context.SaveChanges();
+
+            return Ok(payment);
+        }
+
+    }
+}
---
+++ AspAPI/ExamBanking/ExamBanking/DTO/PaymentDto/CreatePaymentRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamBanking.DTO.PaymentDto
+{
+    public class CreatePaymentRequest
+    {
+       
+        public DateTime? Paydate { get; set; }
+        public string? Paycontent { get; set; }
+        public int? Money { get; set; }
+        public int? Status { get; set; }
+       
+    }
+}
